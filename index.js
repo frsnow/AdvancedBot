21e8f2c75600427c7c84d15b5cd0c2ffc3a4d75c
Aklox <akloxdev@gmail.com>
2023-11-14 03:50:14 +0100

    Fix index.js file

--- index.js
+++ index.js
@@ -2,15 +2,14 @@ const { Client, IntentsBitField, Collection } = require('discord.js');
 const client = new Client({
     intents: new IntentsBitField(3276799)
 })
-const dotenv = require('dotenv');
-dotenv.config();
+const loadEvents = require("./interfaces/IEvents");
+const loadCommands = require("./interfaces/ICommand");
+require('dotenv').config();
 
 client.commands = new Collection();
-client.events = new Collection();
 
-require("colors");
-const messageReady = "Bot is ready to use!";
-
-client.login(process.env.TOKEN).then(() => {
-    console.log("[LOGS] ".blue +messageReady.green);
-})
+(async () => {
+    await loadEvents(client);
+    await loadCommands(client);
+    await client.login(process.env.TOKEN);
+})();
