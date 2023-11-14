1b142cac1bdbe4c92c915e1a1aaafebf0814d088
Aklox <akloxdev@gmail.com>
2023-11-14 04:01:39 +0100

    Add guildDelete events

---
+++ events/guilds/guildDelete.js
@@ -0,0 +1,19 @@
+const { Events, EmbedBuilder } = require('discord.js');
+const updateChannelStats = require('../../utils/functions/updateStats');
+const config = require("../../utils/config");
+
+module.exports = {
+    name: Events.GuildDelete,
+
+    async run (client, guild) {
+        console.log(`[LOGS] `.blue + `Left a guild: ${guild.name}`.grey + ` have ${guild.memberCount} members (${guild.id})`.grey);
+        console.log(`[UPDATE]`.yellow + ` Updating server count channel (${client.guilds.cache.size})`.grey)
+        updateChannelStats(client);
+
+
+        let channel = client.channels.cache.get(config.Main_GuildChannel.ChannelGuildRemove);
+        if (!channel) return;
+
+        TODO: "Add embed for v1.0.2"
+    }
+}
