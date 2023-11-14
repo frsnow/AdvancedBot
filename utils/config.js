19b835d7483d8be72184265d43b7ca9b2bc70313
Aklox <akloxdev@gmail.com>
2023-11-14 23:57:47 +0100

    Add mainDeveloper in config

--- utils/config.js
+++ utils/config.js
@@ -1,5 +1,6 @@
 module.exports = {
-    Version: "1.0.0",
+    Version: "1.0.1",
+    MainDeveloper: ["1023706648193290292", "895315953401884722"],
     Main_GuildChannel: {
         ChannelGuildRemove: "1173817667220688907",
         ChannelGuildAdd: "1173817652855197797",
