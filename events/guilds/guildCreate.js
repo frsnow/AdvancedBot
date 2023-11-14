59df40a22463510ecbd2357579a323ff595e55c2
Aklox <akloxdev@gmail.com>
2023-11-14 04:01:27 +0100

    Add guildCreate events

---
+++ events/guilds/guildCreate.js
@@ -0,0 +1,17 @@
+const { Events, EmbedBuilder } = require('discord.js');
+const updateChannelStats = require("../../utils/functions/updateStats");
+
+module.exports = {
+    name: Events.GuildCreate,
+
+    async run (client, guild) {
+        console.log(`[LOGS] `.blue + `Joined a new guild: ${guild.name}`.grey + ` have ${guild.memberCount} members (${guild.id})`.grey);
+        console.log(`[UPDATE]`.yellow + ` Updating server count channel (${client.guilds.cache.size})`.grey)
+        updateChannelStats(client);
+
+        let channel = client.channels.cache.get(config.Main_GuildChannel.ChannelGuildAdd);
+        if (!channel) return;
+
+        TODO: "Add embed for v1.0.2"
+    }
+}
