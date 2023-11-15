1e03122afc60303987464779782ef289724e43bc
Aklox <akloxdev@gmail.com>
2023-11-15 06:00:13 +0100

    Add delete guild from database on guildDelete event

--- events/guilds/guildDelete.js
+++ events/guilds/guildDelete.js
@@ -1,11 +1,14 @@
-const { Events, EmbedBuilder } = require('discord.js');
+const { EmbedBuilder } = require('discord.js');
 const updateChannelStats = require('../../utils/functions/updateStats');
 const config = require("../../utils/config");
+const database = require("../../utils/structure/mysql");
 
 module.exports = {
-    name: Events.GuildDelete,
+    name: (Events) => {
+        return Events.GuildDelete;
+    },
 
-    run: (client, guild) => {
+    run: async (Discord, client, guild) => {
         console.log(`[LOGS] `.blue + `Left a guild: ${guild.name}`.grey + ` have ${guild.memberCount} members (${guild.id})`.grey);
         console.log(`[UPDATE]`.yellow + ` Updating server count channel (${client.guilds.cache.size})`.grey)
         updateChannelStats(client);
@@ -14,6 +17,16 @@ module.exports = {
         let channel = client.channels.cache.get(config.Main_GuildChannel.ChannelGuildRemove);
         if (!channel) return;
 
-        TODO: "Add embed for v1.0.2"
+        let embed = new EmbedBuilder()
+            .setTitle(`Left a guild`)
+            .setTimestamp()
+            .setThumbnail(guild.iconURL({ dynamic: true }))
+            .setFooter({ text: "AdvancedBot", iconURL: guild.iconURL({ dynamic: true }) })
+            embed.setDescription(`\`\`\`diff\n- ${guild.name} (${guild.id})\n\`\`\`The guild have ${guild.memberCount} members`)
+
+        channel.send({ embeds: [embed] })
+        await database.query(`DELETE FROM guilds WHERE guild_id = ?`, [guild.id]).then(() => {
+            console.log(`[DATABASE] `.green + `Removed guild ${guild.name} (${guild.id}) from database`.grey);
+        })
     }
 }
