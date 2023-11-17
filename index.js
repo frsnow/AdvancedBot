e980e18e5df1e941280bd23ee6c1be365fa25ea5
Aklox <akloxpro@gmail.com>
2023-11-17 12:01:34 +0100

    Add subCommand collection

--- index.js
+++ index.js
@@ -1,21 +1,22 @@
 const { Client, IntentsBitField, Collection } = require('discord.js');
 const client = new Client({
     intents: new IntentsBitField(3276799),
+    shards : [0, 1, 2],
+    shardCount : 3 
 })
+require('dotenv').config();
 const loadEvents = require("./interfaces/IEvents");
 const loadCommands = require("./interfaces/ICommand");
 const updateChannelStats = require("./utils/functions/updateStats");
-require('dotenv').config();
 
 client.commands = new Collection();
-// client.subCommands = new Collection();
+client.subCommands = new Collection();
 
 (async () => {
     await loadEvents(client);
     await loadCommands(client);
     await client.login(process.env.TOKEN);
+    setInterval(() => {
+        updateChannelStats(client);
+    }, 1000 * 60 * 25)
 })();
-
-setInterval(() => {
-    updateChannelStats(client);
-}, 1000 * 60 * 30)
