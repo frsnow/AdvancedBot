0cd87a2767c27a24bb999fbf3928b83aa10e672f
Aklox <akloxpro@gmail.com>
2023-11-17 12:01:44 +0100

    Add messageDelete event

--- events/logs/messageDelete.js
+++ events/logs/messageDelete.js
@@ -4,5 +4,11 @@ module.exports = {
     },
 
     run: async (Discord, client, interaction) => {
+        let message = interaction;
+        if (message.author.bot) return;
+        if (message.channel.type === "dm") return;
+        let guild = client.guilds.cache.get(message.guildId);
+
+        console.log(`[LOGS] `.blue + `Message deleted in ${guild.name} (${guild.id}), message: ${message}`.green);
     }
 }
