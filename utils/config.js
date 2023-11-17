2d280df29c13017bd34046dc0b84bbce4d9bf812
Aklox <akloxpro@gmail.com>
2023-11-17 12:03:23 +0100

    Add new config in config.js

--- utils/config.js
+++ utils/config.js
@@ -1,7 +1,13 @@
 module.exports = {
     Version: "1.0.2",
+    Icons: {
+        Yes: "<:yes_check:1173781550433833040>",
+        No: "<:no_check:1173781592607572039>"
+    },
     MainDeveloper: ["1023706648193290292", "895315953401884722"],
     Main_Guild: {
+        ID: "1173700860228931694",
+        StaffIDRole: "1173722553819353109",
         ChannelLogs: {
             ChannelGuildRemove: "1173817667220688907",
             ChannelGuildAdd: "1173817652855197797",
